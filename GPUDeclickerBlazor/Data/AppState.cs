4ba69f50743d2a4d87e06715c4501fc206344b4d
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-03-02 12:41:58 -0400

    Add AppState as singleton #1

---
+++ GPUDeclickerBlazor/Data/AppState.cs
@@ -0,0 +1,20 @@
+﻿using GPUDeclickerUWP.Model.Data;
+using System;
+
+namespace GPUDeclickerBlazor.Data
+{
+    public class AppState
+    {
+        public AudioData AudioData { get; private set; }
+
+        public event Action OnChange;
+
+        public void SetAudioData(AudioData audioData)
+        {
+            AudioData = audioData;
+            NotifyDataChanged();
+        }
+
+        private void NotifyDataChanged() => OnChange?.Invoke();
+    }
+}
