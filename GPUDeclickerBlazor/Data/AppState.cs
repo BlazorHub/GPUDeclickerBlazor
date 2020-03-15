e31620dcb9313ae6e316c511671f648bec4f11aa
DmitriiKh <dmitrii.khrustalev@gmail.com>
2020-03-15 08:21:20 -0300

    Refactoring (audio settings to AppState) #1

--- GPUDeclickerBlazor/Data/AppState.cs
+++ GPUDeclickerBlazor/Data/AppState.cs
@@ -5,16 +5,55 @@ namespace GPUDeclickerBlazor.Data
 {
     public class AppState
     {
+        const float _defaultThreshold = 10f;
+        const int _defaultMaxLength = 250;
+
+        // Audio processing parameters
+        public float Threshold
+        {
+            get
+            {
+                return AudioData is null 
+                    ? _defaultThreshold 
+                    : AudioData.AudioProcessingSettings.ThresholdForDetection;
+            }
+            set
+            {
+                if (AudioData != null)
+                    AudioData.AudioProcessingSettings.ThresholdForDetection = value;
+            }
+        }
+
+        public int MaxLength
+        {
+            get
+            {
+                return AudioData is null
+                    ? _defaultMaxLength
+                    : AudioData.AudioProcessingSettings.MaxLengthOfCorrection;
+            }
+            set
+            {
+                if (AudioData != null)
+                    AudioData.AudioProcessingSettings.MaxLengthOfCorrection = value;
+            }
+        }
+
         public AudioData AudioData { get; private set; }
 
-        public event Action OnChange;
+        public event Action OnAudioChange;
 
         public void SetAudioData(AudioData audioData)
         {
+            if (audioData is null)
+                return;
+
             AudioData = audioData;
-            NotifyDataChanged();
+            AudioData.AudioProcessingSettings.ThresholdForDetection = _defaultThreshold;
+            AudioData.AudioProcessingSettings.MaxLengthOfCorrection = _defaultMaxLength;
+            NotifyAudioDataChanged();
         }
 
-        private void NotifyDataChanged() => OnChange?.Invoke();
+        private void NotifyAudioDataChanged() => OnAudioChange?.Invoke();
     }
 }
